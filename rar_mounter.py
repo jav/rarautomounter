43f3d4797f6fcf346971ea7e624d504f19ecf893
Javier Ubillos <javier.ubillos@magine.com>
2015-02-22 13:13:38 +0100

    Refactored out the mount(), crate_mountpoint() umount() commands to separate functions.

--- rar_mounter.py
+++ rar_mounter.py
@@ -6,6 +6,8 @@ import file_picker
 #then import system libraries
 import argparse
 import os
+import subprocess
+import sys
 
 
 parser = argparse.ArgumentParser(description='Mount a compress filed as a dir.')
@@ -23,25 +25,83 @@ parser.add_argument('--mount-command',
 						help='Mount command.')
 parser.add_argument('--mount-options', nargs='+',
 						default=['readonly', 'allow_other'],
-						help="Optional arguments for the mount command. Each element has '-o' prepended.")
+						help="Optional arguments for the mount command. Single chars have '-' prepended, longer strings have '-o ' prepended.")
+parser.add_argument('--umount-command',
+						default="fusermount",
+						help="Umount command.")
+parser.add_argument('--umount-options', nargs='+',
+						default=['u', 'z'])
 
 
 
 args = parser.parse_args()
 
-def mount(dest, source, mount_command, mount_options):
-	print "mount(): dest: %s, source: %s" % (dest, source)
+def error(err_str):
+	sys.stderr.write("Error: %s"%(err_str, ))
 
-def symlink(dest, dir_to_scan, ext):
-	print "link(): dest: %s, dir_to_scan: %s, exts: %s" % (dest, dir_to_scan, ext)
+def warn(warn_str):
+	sys.stderr.write("Warning: %s"% (warn_str, ))
+
+def mount(dest, source, mount_command, mount_options, noop):
+	''' Attempt to create a mountpoint and mount the compressed file to there.'''
+	mount_cmd = [mount_command, source, dest]
+	for mount_option in mount_options:
+		if len(mount_option) == 1:
+			mount_cmd.append("-%s"%mount_option)
+		else:
+			mount_cmd.append("-o")
+			mount_cmd.append(mount_option)
+	if noop:
+		print "mount(): subprocess.call(%s)" % (mount_cmd, )
+	if not noop:
+		print "subprocess.call(%s)" % (mount_cmd, )
+		subprocess.call(mount_cmd)
+
+def symlink(dest, dir_to_scan, ext, noop):
+	'''Scan a mountpoint for files with extentions and
+	   create symlinks to them.'''
+	print "found %s files in %s" % (os.listdir(dir_to_scan), dir_to_scan)
+	for f in os.listdir(dir_to_scan):
+		if ext == os.path.splitext(f)[0]:
+			print "link %s as %s" % (f, os.path.join(dest, f))
+
+def create_mountpoint(mountpoint, noop):
+	if len(os.listdir(mountpoint)) != 0:
+		warn("mountpoint (%s) is not empty."%(mountpoint, ))
+		if not noop:
+			os.makedirs(dest)
+		else:
+			print "create dir %s"%mountpoint
+
+def umount(d, umount_command, umount_options, noop):
+	umount_cmd = [umount_command, d]
+	for umount_option in umount_options:
+		if len(umount_option) == 1:
+			umount_cmd.append("-%s"%umount_option)
+		else:
+			umount_cmd.append("-o")
+			umount_cmd.append(mount_option)
+	if noop:
+		print "umount(): subprocess.call(%s)" % (umount_cmd, )
+	if not noop:
+		print "subprocess.call(%s)" % (umount_cmd, )
+		subprocess.call(umount_cmd)
+
+
 
 def main(**kwargs):
 	print "kwargs", kwargs
 	basedir = kwargs['basedir'] #break if this fails
 	noop = kwargs['noop']
+
 	make_links = kwargs['make_links']
+
 	mount_command = kwargs['mount_command']
 	mount_options = kwargs['mount_options']
+
+	umount_command = kwargs['umount_command']
+	umount_options = kwargs['umount_options']
+
 
 
 	fp = file_picker.FilePicker()
@@ -52,10 +112,16 @@ def main(**kwargs):
 		full_file_name = os.path.join(dir_name, file_name)
 		mountpoint = "%s.mountpoint" % full_file_name
 		source = full_file_name
-		mount(mountpoint, source)
+		
+		create_mountpoint(mountpoint, noop)
+		if os.listdir(mountpoint) > 0:
+			umount(mountpoint, umount_command, umount_options, noop)
+
+		mount(mountpoint, source, mount_command, mount_options, noop)
+
 		for ext in make_links:
 			dir_to_scan = mountpoint
-			symlink(dir_name, mountpoint, ext)
+			symlink(dir_name, mountpoint, ext, noop)
 
 
 	
