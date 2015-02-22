d6a5e8a70f3dc701df0fb36298a430193f68a16d
Javier Ubillos <javier.ubillos@magine.com>
2015-02-22 13:36:24 +0100

    Added a few video files as default for --make-links.

--- rar_mounter.py
+++ rar_mounter.py
@@ -15,7 +15,7 @@ parser.add_argument('-d', '--base-dir', dest="basedir",
 						required=True,
 						help='Base directory.')
 parser.add_argument('-l', '--make-links', nargs='+',
-						default=[],
+						default=['avi', 'mkv', 'mp4'],
 						help='Automatically add a link to any files that matches fileendings (e.g. "--make-link avi mkv mp4")')
 parser.add_argument('-n', dest='noop', action='store_true',
 						default=False,
