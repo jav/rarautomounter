40148976eedd8a2b987be05a7fb6d9ac41484da4
Javier Ubillos <javier.ubillos@magine.com>
2015-02-24 07:23:32 +0100

    Make arguments for -l optional.

--- rar_mounter.py
+++ rar_mounter.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
 	parser.add_argument('-d', '--base-dir', dest="basedir",
 							required=True,
 							help='Base directory.')
-	parser.add_argument('-l', '--make-links', nargs='+',
+	parser.add_argument('-l', '--make-links', nargs='*',
 							default=['avi', 'mkv', 'mp4'],
 							help='Automatically add a link to any files that matches fileendings (e.g. "--make-link avi mkv mp4")')
 	parser.add_argument('-n', dest='noop', action='store_true',
